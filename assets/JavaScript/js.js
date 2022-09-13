676eed0d02466f7b58074ec9c27d0cce541aae03
Alejandro Trikitrakatelas Sanchez Cano <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2022-09-13 02:25:55 -0500

    Actualización 0.0.6

--- assets/JavaScript/js.js
+++ assets/JavaScript/js.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.hamburger');
 
-hamburger.addEventListener('click',function(){
+hamburger.addEventListener('click', ()=> {
     this.classList.toggle('is-active');
 });
